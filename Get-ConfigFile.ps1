da3b3df6283086811156d664ecdd447c09c5b825
David Walker <dave@sitecoredave.com>
2022-04-28 12:13:27 -0500

    init

---
+++ Get-ConfigFile.ps1
@@ -0,0 +1,70 @@
+#Set-StrictMode -Version Latest
+#####################################################
+# Get-ConfigFile
+#####################################################
+<#PSScriptInfo
+
+.VERSION 0.1
+
+.GUID 01002a90-c61d-4475-ad98-7369907e54e3
+
+.AUTHOR David Walker, Sitecore Dave, Radical Dave
+
+.COMPANYNAME David Walker, Sitecore Dave, Radical Dave
+
+.COPYRIGHT David Walker, Sitecore Dave, Radical Dave
+
+.TAGS powershell sitecore package
+
+.LICENSEURI https://github.com/SharedSitecore/Get-ConfigFile/blob/main/LICENSE
+
+.PROJECTURI https://github.com/SharedSitecore/Get-ConfigFile
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ PowerShell Script to Get-ConfigFile
+
+.PARAMETER name
+Path of package
+
+#> 
+#####################################################
+# Get-ConfigFile
+#####################################################
+
+[CmdletBinding(SupportsShouldProcess)]
+Param(
+	[Parameter(Mandatory=$true)]
+	[string] $name
+)
+begin {
+	$ProgressPreference = "SilentlyContinue"		
+	$ErrorActionPreference = 'Stop'
+	$PSScriptName = ($MyInvocation.MyCommand.Name.Replace(".ps1",""))
+	$PSCallingScript = if ($MyInvocation.PSCommandPath) { $MyInvocation.PSCommandPath | Split-Path -Parent } else { $null }
+	Write-Verbose "$PSScriptRoot\$PSScriptName $name called by:$PSCallingScript"
+}
+process {
+	if (!$results -and (Test-Path "*.$name*"))
+	{
+		if (Test-Path "*.$name.user") { $results = ".$name.user" } 
+		elseif (Test-Path ".$name") { $results = ".$name"}
+	}
+	return $results
+}
+end {
+	Write-Verbose "$PSScriptName $name end"
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Get-ConfigFile
+## Description
 Powershell Helper for Scripts to find/use config files
+
+## Installation (Powered by [PowerShellGallery](https://powershellgallery.com/packages/Get-ConfigFile))
+PS> ```Install-Script Get-ConfigFile```
+
+PS> Install-Script -Name Get-ConfigFile
+
+## Example
+PS> ```Get-ConfigFile``` (-help)
+
+PS> ```Get-ConfigFile name```
+
+## Release Notes
+- 0.1 init
+
+## Copyright
+David Walker, [Radical Dave](https://github.com/radical-dave), [Sitecore Dave](https://github.com/sitecoredave)
+
+## License
+MIT License: https://github.com/Radical-Dave/Get-ConfigFile/blob/main/LICENSE
